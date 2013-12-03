b46999329bc8278e33be44667e14b135c00b2319
Maciej Kaczor <hangeyes@gmail.com>
2013-12-03 21:35:56 +0100

    events and keyboard

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys, pygame
+from pygame.constants import *
+
 pygame.init()
 
 size = width, height = 800, 600
 black = 0,0,0
+stop = False
 
 screen = pygame.display.set_mode(size)
 rectangle = pygame.Rect(0,50,100,100)
 rectangle2 = pygame.Rect(width,50,100,100)
+
+def setStop(bool):
+    global stop
+    stop = bool
 
 def moveRect1():
     rectangle.left +=1
@@ -23,21 +30,28 @@ def drawScreen():
 def updateLogic():
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_SPACE:
+                setStop(True)
+        elif event.type == pygame.KEYUP:
+            if event.key == K_SPACE:
+                setStop(False)
 
-    if rectangle.right == width:
-        moveRect1()
-        rectangle2.right = 0
-    elif rectangle.left <= width:
-        moveRect1()
-        moveRect2()
-    elif rectangle2.right < width:
-        moveRect2()
-    elif rectangle2.right == width:
-        moveRect2()
-        rectangle.right = 0
-    else:
-        moveRect2()
-        moveRect1()
+    if stop == False:
+        if rectangle.right == width:
+            moveRect1()
+            rectangle2.right = 0
+        elif rectangle.left <= width:
+            moveRect1()
+            moveRect2()
+        elif rectangle2.right < width:
+            moveRect2()
+        elif rectangle2.right == width:
+            moveRect2()
+            rectangle.right = 0
+        else:
+            moveRect2()
+            moveRect1()
 
 while 1:
         drawScreen()
