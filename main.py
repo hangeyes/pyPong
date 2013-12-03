054f1f72242d96b0c430e956ba7ca818c1cfe053
Maciej Kaczor <hangeyes@gmail.com>
2013-12-03 21:01:42 +0100

    rectangle and movement test

--- main.py
+++ main.py
@@ -5,11 +5,40 @@ size = width, height = 800, 600
 black = 0,0,0
 
 screen = pygame.display.set_mode(size)
-rectangle = pygame.Rect()
+rectangle = pygame.Rect(0,50,100,100)
+rectangle2 = pygame.Rect(width,50,100,100)
 
-while 1:
+def moveRect1():
+    rectangle.left +=1
+
+def moveRect2():
+    rectangle2.left +=1
+
+def drawScreen():
+    screen.fill(pygame.Color(255,0,0,255))
+    pygame.draw.rect(screen,black,rectangle,5)
+    pygame.draw.rect(screen,black,rectangle2,0)
+    pygame.display.flip()
+
+def updateLogic():
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    screen.fill(pygame.Color(255,0,0,255))
-    pygame.display.flip()
+    if rectangle.right == width:
+        moveRect1()
+        rectangle2.right = 0
+    elif rectangle.left <= width:
+        moveRect1()
+        moveRect2()
+    elif rectangle2.right < width:
+        moveRect2()
+    elif rectangle2.right == width:
+        moveRect2()
+        rectangle.right = 0
+    else:
+        moveRect2()
+        moveRect1()
+
+while 1:
+        drawScreen()
+        updateLogic()
